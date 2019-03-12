d3f66a87ab0c5460823dce9583b0c4650ed1d5d2
Luca Silvestri <luca.silvestri@ecogriddy.com>
2019-03-12 15:07:38 +0100

    Deleted gyro calibration as dynamic per its nature

--- mpu6050.js
+++ mpu6050.js
@@ -67,15 +67,7 @@ class MPU6050 {
         calibration.accel[key] = -element;
       }
     }
-    //Evaluates the calibration values for gyro
-    for (const key in staticMeasure.gyro) {
-      if (staticMeasure.accel.hasOwnProperty(key)) {
-        const element = staticMeasure.accel[key];
-        calibration.gyro[key] = -element;
-      }
-    }
     this.sensor.calibrateAccel(calibration.accel);
-    this.sensor.calibrateGyro(calibration.gyro);
   }
 }
 
